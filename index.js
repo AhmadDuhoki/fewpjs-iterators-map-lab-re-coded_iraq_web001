9882cea0ff574d4ee8a6afcc05b3a8af74a855c2
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-06-15 12:51:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const tutorials = [
 
 const titleCased = () => {
   return tutorials.map(tutorial => {
-    tutorial.split(" ").map(word => {
+    tutorial.split(" ")
+    .map(word => {
       return word.toLowerCase().charAt(0).toUpperCase() + word.slice(1).join(" ")
     })
   })
